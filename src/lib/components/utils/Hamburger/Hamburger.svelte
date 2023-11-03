30396df403b543037402b72739106f21622da0f4
murithicollins <murithic2016@gmail.com>
2023-11-03 11:48:12 +0300

    navbar hamburger changes

--- src/lib/components/utils/Hamburger/Hamburger.svelte
+++ src/lib/components/utils/Hamburger/Hamburger.svelte
@@ -1,17 +1,14 @@
 <script>
-	export let open = false
+	export let open = false;
+	import hamburger from "$lib/assets/images/hamburger.png"
 </script>
 
 <button class="text-black md:hidden flex hover:text-gray-700 cursor-pointer mr-4 border-none focus:outline-none" class:open on:click={() => open = !open}>
-	<svg width=24 height=24>
-		<line id="top" x1=0 y1=2  x2=20 y2=2/>
-		<line id="middle" x1=0 y1=10 x2=10 y2=10/>
-		<line id="bottom" x1=0 y1=18 x2=20 y2=18/>
-	</svg>
+	<img src={hamburger} alt="Logo" class="md:h-8 h-6 md:w-8 w-10"/>
 </button>
 
 <style>
-	svg {
+	/* svg {
 		min-height: 24px;
 		transition: transform 0.3s ease-in-out;
 	}
@@ -21,12 +18,12 @@
 		stroke-width: 3;
 		transition: transform 0.3s ease-in-out
 	}
-	
+	 */
 	button {
 		z-index: 20;
 	}
 	
-	.open svg {
+	/* .open svg {
 		transform: scale(0.7)
 	}
 	
@@ -40,5 +37,5 @@
 	
   .open #bottom {
 		transform: translate(-12px, 9px) rotate(-45deg)
-	}
+	} */
 </style>
